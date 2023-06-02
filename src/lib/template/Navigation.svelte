f932a92ece96a5e1a385f34fd35e8eff52a93fb2
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2023-06-02 10:11:33 -0600

    add blog to menu

--- src/lib/template/Navigation.svelte
+++ src/lib/template/Navigation.svelte
@@ -4,5 +4,6 @@
 
 <div class="bg-surface-200 text-surface-800 dark:bg-surface-800 dark:text-surface-200">
     <a href="{base}/">Home</a>
+    <a href="{base}/blog">Blog</a>
     <a href="{base}/about">About</a>
 </div>
